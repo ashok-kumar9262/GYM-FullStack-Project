a27c55fd010b5e09107fbb6e0acfde982db89bac
ashok <ashok-kumar9262>
2026-03-05 21:50:13 +0530

    url changes

--- vite-Backend/app.js
+++ vite-Backend/app.js
@@ -27,7 +27,7 @@ console.log("PORT:", process.env.PORT);
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "https://gym-fullstack-project.onrender.com/send/mail",
     methods: ["POST"],
     credentials: true,
   })
