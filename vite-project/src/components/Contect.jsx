ab1efc4da7c58a3b05d58f485e2d50d263307a80
ashok <ashok-kumar9262>
2026-03-06 13:37:26 +0530

    Url and code

--- vite-project/src/components/Contect.jsx
+++ vite-project/src/components/Contect.jsx
@@ -17,7 +17,7 @@ const Contect = () => {
     try {
       // Make sure the URL is correct
       const { data } = await axios.post(
-        'https://gym-full-stack-project-15pj.vercel.app',  // Backend URL
+        'https://gym-full-stack-project-15pj.vercel.app', "http://localhost:4000",  // Backend URL
         { name, email, message },
         {
           withCredentials: true, // Ensure cookies are sent if needed
